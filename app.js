b59b170d0b4db89d8b18ffcba314674dadee1829
itscsp <cspoojary7972@gmail.com>
2021-09-28 14:42:51 +0530

    class is focus in v-bind method

--- app.js
+++ app.js
@@ -2,21 +2,14 @@ var  app = new Vue({
     el: '#app',
     data:{
         message: 'hello I am Chethan S Poojary',
-        intro: '<h1>Ya I am Learning <strong>Vue</strong></h1>',
-        view1: false,
-        view2: true,
+        title: "you hover me on"+ new Date(),
+        url: "http://vuejs.org/images/logo.png",
+        isActive: true,
     }
 })
 /*
 Notes:
-v-text = display data text
-v-html = insert complete html tags and text into a DOM
-v-show = this like show and hide functionality in Jquery
-v-if = if true it will show if false it will hide it
-v-else there no need to write conditionalEnd
-
-v-once = this render data once if we update data ofter that it will won't change it
-
-v-clock : with this we can make loading functionality
-
+Class and Style Bindings
+v-bind :' use to add dyanamic data attribute, class amd images and so on
+** shortcut for v-bind :src="" **
 */
