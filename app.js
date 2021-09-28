3fc4d78c0a0e3f00fa30e3254ec8fe4cbd95f3db
itscsp <cspoojary7972@gmail.com>
2021-09-28 14:16:10 +0530

    this section we did vue directive ecercises

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 var  app = new Vue({
     el: '#app',
     data:{
-        message: 'hello Vue world'
+        message: 'hello I am Chethan S Poojary',
+        intro: '<h1>Ya I am Learning <strong>Vue</strong></h1>',
+        view1: false,
+        view2: true,
     }
 })
+/*
+Notes:
+v-text = display data text
+v-html = insert complete html tags and text into a DOM
+v-show = this like show and hide functionality in Jquery
+v-if = if true it will show if false it will hide it
+v-else there no need to write conditionalEnd
+
+v-once = this render data once if we update data ofter that it will won't change it
+
+v-clock : with this we can make loading functionality
+
+*/
